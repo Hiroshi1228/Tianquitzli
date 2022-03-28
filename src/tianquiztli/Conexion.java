89584475ab63e364bb1f875c154c6b3727fd5696
DELL <DELL@192.168.1.72>
2022-03-28 17:38:03 -0600

    Proyecto subido al repositorio

---
+++ src/tianquiztli/Conexion.java
@@ -0,0 +1,29 @@
+package tianquiztli;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+    private final String base = "tianquiztli";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/" + base;
+    private Connection con = null;
+    
+    public Connection getConnection(){
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return con;
+    }
+}
